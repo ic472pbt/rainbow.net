9dc25686660e0d3a326773e7ec322743607db480
Pavel Tatarintsev <ic472pbt@ya.ru>
2025-02-28 10:43:47 +0400

    Chebyshev tests

---
+++ src/Rainbow.NET/Chebyshev.fs
@@ -0,0 +1,9 @@
+﻿namespace Rainbow.NET
+module Chebyshev =
+    let C n k =
+        if k > n then 0
+        elif k = 0 || k = n then 1
+        else
+            let k = min k (n - k) // Use symmetry C(n, k) = C(n, n-k) to reduce calculations
+            seq {1..k} 
+                |> Seq.fold (fun acc i -> acc * (n - i + 1) / i) 1
---
+++ tests/LibTests/ChebyshevTests.fs
@@ -0,0 +1,39 @@
+﻿namespace LibTests
+module ChebyshevTests =
+    open System
+    open System.Numerics
+    open Rainbow.NET
+    open NUnit.Framework
+    open FsCheck
+    open FsCheck.NUnit
+
+    [<Literal>]
+    let maxN = 20
+
+    [<SetUp>]
+    let Setup () =
+        ()
+
+    [<Test>]
+    let symmetryProperty ([<Range(1,1,maxN)>] n) =
+        for k = 0 to n do
+            let leftSide = Chebyshev.C n k
+            let rightSide = Chebyshev.C n (n - k)
+            Assert.AreEqual(leftSide, rightSide)
+
+    [<Test>]
+    let sumProperty ([<Range(1,1,maxN)>] n) =
+        for k = 1 to n do
+            let leftSide = Chebyshev.C n k
+            let rightSide = Chebyshev.C (n-1) (k-1) + Chebyshev.C (n-1) k
+            Assert.AreEqual(leftSide, rightSide)
+
+    [<Test>]
+    let totalNumberProperty ([<Range(1,1,maxN)>] n) =
+        let total = [0..n] |> List.sumBy (fun k -> Chebyshev.C n k)
+        Assert.AreEqual(total, pown 2 n)
+
+    [<Test>]
+    let evenSumProperty ([<Range(1,1,maxN)>] n) =
+        let total = [0..n] |> List.sumBy (fun k -> Chebyshev.C n (2*k))
+        Assert.AreEqual(total, pown 2 (n-1))
